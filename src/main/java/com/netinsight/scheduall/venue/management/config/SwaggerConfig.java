9e4308b1dad2429d514fdef57d394fd1b0693d17
Ignatius Joseph <ignatiusdeepu@gmail.com>
2019-05-30 16:40:17 -0400

    initial commit

--- src/main/java/com/netinsight/scheduall/venue/management/config/SwaggerConfig.java
+++ src/main/java/com/netinsight/scheduall/venue/management/config/SwaggerConfig.java
@@ -1,5 +1,24 @@
 package com.netinsight.scheduall.venue.management.config;
 
+import static com.netinsight.scheduall.venue.management.constants.SwaggerConstants.SWAGGER_UI;
+import static com.netinsight.scheduall.venue.management.constants.SwaggerConstants.WEB_JARS;
+import static com.netinsight.scheduall.venue.management.constants.SwaggerConstants.RESOURCE_LOCATION;
+import static com.netinsight.scheduall.venue.management.constants.SwaggerConstants.WEB_JARS_LOCATION;
+import static com.netinsight.scheduall.venue.management.constants.SwaggerConstants.AUTHORIZATION;
+import static com.netinsight.scheduall.venue.management.constants.SwaggerConstants.TOKEN_MSG;
+import static com.netinsight.scheduall.venue.management.constants.SwaggerConstants.TYPE_STR;
+import static com.netinsight.scheduall.venue.management.constants.SwaggerConstants.HEADER;
+import static com.netinsight.scheduall.venue.management.constants.SwaggerConstants.CONTENT_TYPE;
+import static com.netinsight.scheduall.venue.management.constants.SwaggerConstants.API_NAME;
+import static com.netinsight.scheduall.venue.management.constants.SwaggerConstants.API_DESCRIPTION;
+import static com.netinsight.scheduall.venue.management.constants.SwaggerConstants.VERSION;
+import static com.netinsight.scheduall.venue.management.constants.SwaggerConstants.EMPTY_STRING;
+import static com.netinsight.scheduall.venue.management.constants.SwaggerConstants.NET_INSIGHT;
+import static com.netinsight.scheduall.venue.management.constants.SwaggerConstants.NET_INSIGHT_URL;
+import static com.netinsight.scheduall.venue.management.constants.SwaggerConstants.NET_INSIGHT_EMAIL;
+import static com.netinsight.scheduall.venue.management.constants.SwaggerConstants.ERROR_PATH;
+import static com.netinsight.scheduall.venue.management.constants.SwaggerConstants.ACTUATOR_PATH;
+import static com.netinsight.scheduall.venue.management.constants.SwaggerConstants.BACK_SLASH;
 import java.util.ArrayList;
 import java.util.Arrays;
 
@@ -31,27 +50,27 @@ public class SwaggerConfig implements WebMvcConfigurer {
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
 
 		WebMvcConfigurer.super.addResourceHandlers(registry);
-		registry.addResourceHandler("swagger-ui.html").addResourceLocations("classpath:/META-INF/resources/");
-		registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
+		registry.addResourceHandler(SWAGGER_UI).addResourceLocations(RESOURCE_LOCATION);
+		registry.addResourceHandler(WEB_JARS).addResourceLocations(WEB_JARS_LOCATION);
 	}
 
 	@Bean
 	public Docket api() {
-		final Parameter autherizationParam = new ParameterBuilder().name("Authorization")
-				.description("Enter Bearer token here").modelRef(new ModelRef("string")).parameterType("header")
+		final Parameter autherizationParam = new ParameterBuilder().name(AUTHORIZATION)
+				.description(TOKEN_MSG).modelRef(new ModelRef(TYPE_STR)).parameterType(HEADER)
 				.required(false).build();
-		Parameter headerParam = new ParameterBuilder().name("Content-Type").modelRef(new ModelRef("string")).parameterType("header").required(true).defaultValue(MediaType.APPLICATION_JSON_VALUE).build();
+		Parameter headerParam = new ParameterBuilder().name(CONTENT_TYPE).modelRef(new ModelRef(TYPE_STR)).parameterType(HEADER).required(true).defaultValue(MediaType.APPLICATION_JSON_VALUE).build();
         
 		return new Docket(DocumentationType.SWAGGER_2).select().apis(RequestHandlerSelectors.any())
-				.paths(PathSelectors.any()).paths(Predicates.not(PathSelectors.regex("/error.*"))).paths(Predicates.not(PathSelectors.regex("/actuator.*")))
+				.paths(PathSelectors.any()).paths(Predicates.not(PathSelectors.regex(ERROR_PATH))).paths(Predicates.not(PathSelectors.regex(ACTUATOR_PATH)))
 				.build().apiInfo(metadata())
-				.pathMapping("/").globalOperationParameters(Arrays.asList(headerParam,autherizationParam));
+				.pathMapping(BACK_SLASH).globalOperationParameters(Arrays.asList(headerParam,autherizationParam));
 	}
 
 	private ApiInfo metadata() {
-		final ApiInfo apiInfo = new ApiInfo("Venue Management API on Oracle Cloud", "Venue Management API", "1.0", "",
-				new springfox.documentation.service.Contact("NetInsight", "https://netinsight.com/", "info@netinsight.com"),
-				"NetInsight", "", new ArrayList<VendorExtension>());
+		final ApiInfo apiInfo = new ApiInfo(API_DESCRIPTION, API_NAME, VERSION, EMPTY_STRING,
+				new springfox.documentation.service.Contact(NET_INSIGHT, NET_INSIGHT_URL, NET_INSIGHT_EMAIL),
+				NET_INSIGHT, EMPTY_STRING, new ArrayList<VendorExtension>());
 		return apiInfo;
 	}
 
---
+++ src/main/java/com/netinsight/scheduall/venue/management/constants/SwaggerConstants.java
@@ -0,0 +1,24 @@
+package com.netinsight.scheduall.venue.management.constants;
+
+public interface SwaggerConstants {
+
+	String SWAGGER_UI = "swagger-ui.html";
+	String WEB_JARS = "/webjars/**";
+	String RESOURCE_LOCATION = "classpath:/META-INF/resources/";
+	String WEB_JARS_LOCATION = "classpath:/META-INF/resources/webjars/";
+	String AUTHORIZATION = "Authorization";
+	String TOKEN_MSG = "Enter Bearer token here";
+	String TYPE_STR = "string";
+	String HEADER = "header";
+	String CONTENT_TYPE = "Content-Type";
+	String API_NAME = "Venue Management API";
+	String API_DESCRIPTION = "Venue Management API on Oracle Cloud";
+	String VERSION = "1.0";
+	String EMPTY_STRING = "";
+	String NET_INSIGHT = "NetInsight";
+	String NET_INSIGHT_URL = "https://netinsight.com/";
+	String NET_INSIGHT_EMAIL = "info@netinsight.com";
+	String ERROR_PATH = "/error.*";
+	String ACTUATOR_PATH = "/actuator.*";
+	String BACK_SLASH = "/";
+}
